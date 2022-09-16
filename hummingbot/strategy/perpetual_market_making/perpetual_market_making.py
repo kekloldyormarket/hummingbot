cdfa1c130f769798ef4c767440677058b1c748c2
Jarett Dunn <jarettrsdunn+git@gmail.com>
2022-09-16 00:03:13 -0300

    checker?

--- hummingbot/strategy/perpetual_market_making/perpetual_market_making.py
+++ hummingbot/strategy/perpetual_market_making/perpetual_market_making.py
@@ -507,6 +507,9 @@ class PerpetualMarketMakingStrategy(StrategyPyBase):
                 self.cancel_orders_below_min_spread()
                 if self.to_create_orders(proposal):
                     self.execute_orders_proposal(proposal, PositionAction.OPEN)
+                    self.execute_orders_proposal(proposal, PositionAction.OPEN)
+                    self.execute_orders_proposal(proposal, PositionAction.OPEN)
+                    self.execute_orders_proposal(proposal, PositionAction.CLOSE)
                     self.execute_orders_proposal(proposal, PositionAction.CLOSE)
                 # Reset peak ask and bid prices
                 self._ts_peak_ask_price = market.get_price(self.trading_pair, False)
